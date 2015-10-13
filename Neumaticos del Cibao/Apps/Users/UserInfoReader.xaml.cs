52b1f1a10a84cbc7ab04609ed340101ec3e67ee2
Elainer Toribio <toribioelainer@gmail.com>
2015-10-13 17:40:54 -0400

    IR-17: Created the interface to search and see User Information

--- Neumaticos del Cibao/Apps/Users/UserInfoReader.xaml.cs
+++ Neumaticos del Cibao/Apps/Users/UserInfoReader.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Neumaticos_del_Cibao.CRUD_Permissions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,15 +16,13 @@ using System.Windows.Shapes;
 namespace Neumaticos_del_Cibao
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserInfoReader.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserInfoReader : Page
     {
-        public MainWindow()
+        public UserInfoReader()
         {
             InitializeComponent();
-            UserFrame.Content = new UserInfoReader();
-            
         }
     }
 }
