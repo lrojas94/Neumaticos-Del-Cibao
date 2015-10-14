63f46981bba60c2d8616001d1d2cbd1924227e78
lrojas94 <lrojas94@gmail.com>
2015-10-14 15:41:11 -0400

    IR-19: (Pull Request) Added XmlDoc comments to NoWatermarkDatePicker

--- Neumaticos del Cibao/Apps/Common/NoWatermarkDatePicker.cs
+++ Neumaticos del Cibao/Apps/Common/NoWatermarkDatePicker.cs
@@ -8,6 +8,20 @@ using System.Windows.Controls.Primitives;
 
 namespace Neumaticos_del_Cibao.Apps.Common
 {
+    /// <summary>
+    /// By default, Datepicker's watermark cannot be changed. The idea of creating this class
+    /// is to be able to handle Datepicker watermark easly. Just setting the variable PlaceholderText
+    /// in your Page/Window will suffice to add the watermark you want. Remember to use this class when adding
+    /// a Datepicker.
+    /// 
+    /// E.g:
+    /// <common:NoWatermarkDatePicker PlaceholderText="Inicio de Trabajo" x:Name="startDate" Margin="0,326,10,0" 
+    /// VerticalAlignment="Top" Height="25" HorizontalAlignment="Right" 
+    /// Width="100" Foreground="Black"/>
+    /// 
+    /// Note that PlaceholderText is set to "Inicio de Trabajo", which will appear as watermark for
+    /// this datepicker.
+    /// </summary>
     public class NoWatermarkDatePicker : DatePicker
     {
         Placeholder placeholder;
