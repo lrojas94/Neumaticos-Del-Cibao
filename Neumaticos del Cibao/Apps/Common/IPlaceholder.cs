c6498fd7be010d0e31878c37ffc353dcec40480c
lrojas94 <lrojas94@gmail.com>
2015-10-18 19:11:42 -0400

    IR-20: View and Model are in touch o/

---
+++ Neumaticos del Cibao/Apps/Common/IPlaceholder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neumaticos_del_Cibao.Apps.Common
+{
+    interface IPlaceholder
+    {
+        string PlaceholderText
+        {
+            get;
+            set;
+        }
+        string RealText
+        {
+            get;
+            set;
+        }
+    }
+}
