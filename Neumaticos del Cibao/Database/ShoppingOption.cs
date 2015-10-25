a762d36f6ed4a59436bf396fd91d0242a77c5347
Elainer Toribio <toribioelainer@gmail.com>
2015-10-25 10:56:40 -0400

    IR-39/Shopping Options Database. Create database of the different options to make a purchase. And create a View Class to show the relation between a purchase and class that will work with it

--- Neumaticos del Cibao/Database/ShoppingOption.cs
+++ Neumaticos del Cibao/Database/ShoppingOption.cs
@@ -15,35 +15,27 @@ namespace Neumaticos_del_Cibao.Database
 using System;
     using System.Collections.Generic;
     
-public partial class Employee
+public partial class ShoppingOption
 {
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-    public Employee()
+    public ShoppingOption()
     {
 
-        this.Permissions = new HashSet<Permission>();
+        this.ClassViews = new HashSet<ClassView>();
 
     }
 
 
     public long Id { get; set; }
 
-    public string Username { get; set; }
-
-    public string Password { get; set; }
-
-    public string StartDate { get; set; }
-
-    public long PersonId { get; set; }
+    public string Title { get; set; }
 
 
-
-    public virtual Person Person { get; set; }
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
 
-    public virtual ICollection<Permission> Permissions { get; set; }
+    public virtual ICollection<ClassView> ClassViews { get; set; }
 
 }
 
