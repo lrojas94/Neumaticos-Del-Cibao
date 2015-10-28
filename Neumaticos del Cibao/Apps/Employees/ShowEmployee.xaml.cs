9134ce5cafb733681b11f7e805e04cf9abe8556c
lrojas94 <lrojas94@gmail.com>
2015-10-28 02:01:09 -0400

    IR-17: Added Employee Details view. Note that save/edit/delete from this view is not tested yet.

---
+++ Neumaticos del Cibao/Apps/Employees/ShowEmployee.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neumaticos_del_Cibao.Apps.Employees
+{
+    /// <summary>
+    /// Interaction logic for ShowEmployee.xaml
+    /// </summary>
+    public partial class ShowEmployee : Page
+    {
+        Database.Employee employee;
+        public ShowEmployee(Database.Employee employee)
+        {
+            InitializeComponent();
+            this.employee = employee;
+            DataContext = this.employee;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEmployee(employee));
+        }
+    }
+}
