dcf1ce54826e206ec20c3a86c8b31bf34c030288
Ricardo <ramosortiz95@gmail.com>
2015-10-28 05:28:37 -0400

    IR-23-24: Login Functionality * LoginUI created * Modified the login function and added a class called Encryption storage in Apps/Common * Made some changes in the code. * Fixing some feedbacks about spanish variable naming
    Login:
    	* Added updates to the login file#
    
     IR-23-24/Login:
     	* Removed ShoppingOption files.

---
+++ Neumaticos del Cibao/Apps/Database/DatabaseAddons.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Neumaticos_del_Cibao.Database
+{
+    public partial class databaseEntities : DbContext
+    {
+       public List<Employee> EmployeeSearchByName(string toSearch) 
+            //Basic EmployeeSearch function. For more complex searching use LINQ.
+       {
+            if (toSearch != "")
+            {
+                return Employees.Where(
+                    employee => employee.Username.ToLower().Contains(toSearch)
+                    || employee.Person.Name.ToLower().Contains(toSearch)
+                    || employee.Person.LastName.ToLower().Contains(toSearch)
+                ).ToList();
+            }
+            else
+            {
+                return Employees.ToList();
+            }
+        }
+        public List<Client> ClientSearchByName(string toSearch)
+        {
+            if (toSearch != "")
+            {
+                return Clients.Where(
+                    client => client.Name.ToLower().Contains(toSearch)
+                    || client.ContactName.ToLower().Contains(toSearch)
+                    ).ToList();
+            }
+            else
+            {
+                return Clients.ToList();
+            }
+        }
+    }
+}
