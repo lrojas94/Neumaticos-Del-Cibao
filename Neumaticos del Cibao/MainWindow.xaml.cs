152b1fde1122f707de1ec6d7965d01d616531198
Edward Romero Espinal <edwardespinal16@gmail.com>
2015-11-03 15:42:17 -0400

    IR-33: ViewClient updated
    viewclient only needs the binding, visual is done, also need to add
    charge to the database

--- Neumaticos del Cibao/MainWindow.xaml.cs
+++ Neumaticos del Cibao/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Neumaticos_del_Cibao
         public MainWindow()
         {
             InitializeComponent();
-            employeesFrame.Content = new Apps.Employees.ListEmployee();
+            employeesFrame.Content = new CRUD_Clients.ViewClient();
 
         }
 
