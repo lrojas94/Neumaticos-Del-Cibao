511165a3396ff5a53d2b1159ab59c08f38cb0b2c
Edward Romero Espinal <edwardespinal16@gmail.com>
2015-11-03 21:04:49 -0400

    IR-32: View All Clients * View all clients only need the data binding to be done. * The list box works correctly, so does the filter, buttons still need to be added navigation

--- Neumaticos del Cibao/CRUD_Clients/ShowClient.xaml.cs
+++ Neumaticos del Cibao/CRUD_Clients/ShowClient.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Neumaticos_del_Cibao.CRUD_Permissions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Neumaticos_del_Cibao
+namespace Neumaticos_del_Cibao.CRUD_Clients
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ShowClient.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShowClient : Page
     {
-        public MainWindow()
+        public ShowClient()
         {
             InitializeComponent();
-            employeesFrame.Content = new CRUD_Clients.ViewAllClients();
-
         }
-
-
     }
 }
