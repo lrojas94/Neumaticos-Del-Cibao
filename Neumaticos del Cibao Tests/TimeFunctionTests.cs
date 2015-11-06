28fecc445bd81cc879762b1c06cc9e9efc0d878e
lrojas94 <lrojas94@gmail.com>
2015-11-05 21:06:57 -0400

    GENERAL: Added Testing Project and Framework.

---
+++ Neumaticos del Cibao Tests/TimeFunctionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using System.Timers;
+using Neumaticos_del_Cibao;
+
+namespace Neumaticos_del_Cibao_Tests
+{
+    [TestFixture]
+    public class TimeFunctionTests
+    {
+        [Test]
+        public void TimedFunction_InitializedWithNullAction_ShouldThrowException()
+        {
+            Action action = () => { new Neumaticos_del_Cibao.Apps.Common.TimedFunction(null); };
+            action.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
--- Neumaticos del Cibao/Apps/Common/TimedFunction.cs
+++ Neumaticos del Cibao/Apps/Common/TimedFunction.cs
@@ -7,7 +7,7 @@ using System.Windows.Threading;
 
 namespace Neumaticos_del_Cibao.Apps.Common
 {
-    class TimedFunction
+    public class TimedFunction
     {
         private Action action;
         private DispatcherTimer timer;
@@ -22,12 +22,17 @@ namespace Neumaticos_del_Cibao.Apps.Common
             set
             {
                 TimerInterval = value;
-                this.timer.Interval = TimerInterval;
+                timer.Interval = TimerInterval;
             }
         }
 
         public TimedFunction(Action action,DispatcherTimer timer = null)
         {
+            if(action == null)
+            {
+                throw new ArgumentNullException("Action cannot be null");
+            }
+
             this.action = action;
             this.timer = timer;
 
