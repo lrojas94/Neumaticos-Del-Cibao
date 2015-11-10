b203cb452111d1b1869b86a82683e238db855618
Ricardo <ramosortiz95@gmail.com>
2015-11-09 20:03:58 -0400

    IR-23-24/Login:  	* Removed ShoppingOption files.

--- Database/ShoppingOptionPermissions.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE "ShoppingOptionPermissions" (
-	`ShoppingOptionId`	INTEGER NOT NULL,
-	`PermissionId`	INTEGER NOT NULL,
-	PRIMARY KEY(ShoppingOptionId,PermissionId),
-	FOREIGN KEY(`ShoppingOptionId`) REFERENCES ShoppingOptions ( Id ),
-	FOREIGN KEY(`PermissionId`) REFERENCES Permissions (Id)
-);
-COMMIT;
