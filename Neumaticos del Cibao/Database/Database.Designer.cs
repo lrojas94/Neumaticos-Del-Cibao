3ad55e00f43e53857fa1a4d45d4c55c128678d34
lrojas94 <lrojas94@gmail.com>
2015-11-16 09:40:33 -0400

    GENERAL: Fixed database.edmx file.

--- Neumaticos del Cibao/Database/Database.Designer.cs
+++ Neumaticos del Cibao/Database/Database.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Elainer Toribio\Neumaticos-Del-Cibao\Neumaticos del Cibao\Database\Database.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Universidad\Ingenieria de Software\Neumaticos-Del-Cibao\Neumaticos del Cibao\Database\Database.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
