506258c24dfde141965a28ea5b89340d1bc8270f
Ricardo <ramosortiz95@gmail.com>
2015-11-18 18:39:28 -0400

    IR-44: Roles * UI for assign options to a permission * Funcionality for the assigning of options to a permission * Added PermissionSearchByName to DatabaseAddons.cs

--- Neumaticos del Cibao/Apps/Options/OptionsPermissions.xaml.cs
+++ Neumaticos del Cibao/Apps/Options/OptionsPermissions.xaml.cs
@@ -25,10 +25,11 @@ namespace Neumaticos_del_Cibao.Apps.Options
 
         private Database.databaseEntities database;
         private DispatcherTimer searchTimer;
-        private ObservableCollection<Database.Permission> availablePermissionsList = new ObservableCollection<Database.Permission>(),
-                                          optionPermissionsList = new ObservableCollection<Database.Permission>();
-        private ObservableCollection<Database.Option> availableOptionsList = new ObservableCollection<Database.Option>();
-        private Database.Option selectedOption;
+        private ObservableCollection<Database.Permission> availablePermissionsList = new ObservableCollection<Database.Permission>();
+                                          
+        private ObservableCollection<Database.Option> optionPermissionsList = new ObservableCollection<Database.Option>(),
+                                                      availableOptionsList = new ObservableCollection<Database.Option>();
+        private Database.Permission selectedPermission;
 
         public OptionsPermissions()
         {
@@ -36,7 +37,7 @@ namespace Neumaticos_del_Cibao.Apps.Options
             InitializeComponent();
             database = new Database.databaseEntities();
             availableOptionsList = new ObservableCollection<Database.Option>(database.Options.ToList());
-            options.ItemsSource = database.Options.ToList();
+            options.ItemsSource = availableOptionsList;
             availablePermissionsList = new ObservableCollection<Database.Permission>(database.Permissions.ToList());
             availablePermissions.ItemsSource = availablePermissionsList;
             optionPermissions.ItemsSource = optionPermissionsList;
@@ -55,55 +56,55 @@ namespace Neumaticos_del_Cibao.Apps.Options
 
         private void searchTimer_Tick(object sender, EventArgs e)
         {
-            options.ItemsSource = database.OptionSearchByName(searchBox.RealText);
+            availablePermissions.ItemsSource = database.PermissionSearchByName(searchBox.RealText);
             searchTimer.Stop();
         }
 
-        private void options_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void permissions_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selected = e.AddedItems[0] as Database.Option;
-            selectedOptionLabel.Content = String.Format("Permisos de {0}", selected.OptionTitle);
-            selectedOption = selected;
-            optionPermissionsList = new ObservableCollection<Database.Permission>(selectedOption.Permissions.ToList());
-            availablePermissionsList = new ObservableCollection<Database.Permission>(database.Permissions.ToList().Except(selectedOption.Permissions.ToList()).ToList());
+            var selected = e.AddedItems[0] as Database.Permission;
+            selectedPermissionLabel.Content = String.Format("Opciones de {0}", selected.Name);
+            selectedPermission = selected;
+            optionPermissionsList = new ObservableCollection<Database.Option>(selectedPermission.Options.ToList());
+            availableOptionsList = new ObservableCollection<Database.Option>(database.Options.ToList().Except(selectedPermission.Options.ToList()).ToList());
+            
             optionPermissions.ItemsSource = optionPermissionsList;
-            availablePermissions.ItemsSource = availablePermissionsList;
+            options.ItemsSource = availableOptionsList;
         }
 
         private void availablePermission_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if(selectedOption == null)
+            if(selectedPermission == null)
             {
-                //Alert something here as there's no employee selected.
                 MessageBox.Show(
-                    "Esta tratando de asignar permisos sin haber selecionado una opcion. Seleccione una opcion e intente de nuevo.", 
-                    "Seleccione una Opcion!"
+                    "Esta tratando de asignar opciones sin haber selecionado un permiso. Seleccione un permiso e intente de nuevo.", 
+                    "Seleccione un permiso."
                     );
                 return;
             }
 
-            var selected = (sender as ListBoxItem).Content as Database.Permission;
-            availablePermissionsList.Remove(selected);
+            var selected = (sender as ListBoxItem).Content as Database.Option;
+            availableOptionsList.Remove(selected);
             optionPermissionsList.Add(selected);
 
         }
 
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
-            List<Database.Permission> toRemove = new List<Database.Permission>(),
-                                      toAdd = new List<Database.Permission>();
+            List<Database.Option> toRemove = new List<Database.Option>(),
+                                      toAdd = new List<Database.Option>();
             
-            toRemove = selectedOption.Permissions.ToList().Except(optionPermissionsList.ToList()).ToList();
-            toAdd = optionPermissionsList.ToList().Except(selectedOption.Permissions.ToList()).ToList();
+            toRemove = selectedPermission.Options.ToList().Except(optionPermissionsList.ToList()).ToList();
+            toAdd = optionPermissionsList.ToList().Except(selectedPermission.Options.ToList()).ToList();
 
             foreach(var permission in toRemove)
             {
-                selectedOption.Permissions.Remove(permission);
+                selectedPermission.Options.Remove(permission);
             }
             
             foreach (var permission in toAdd)
             {
-                selectedOption.Permissions.Add(permission);
+                selectedPermission.Options.Add(permission);
             }
 
             try
@@ -120,13 +121,13 @@ namespace Neumaticos_del_Cibao.Apps.Options
         private void optionPermission_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             //Option will never be NULL. 
-            var selected = (sender as ListBoxItem).Content as Database.Permission;
-            availablePermissionsList.Add(selected);
+            var selected = (sender as ListBoxItem).Content as Database.Option;
+            availableOptionsList.Add(selected);
             optionPermissionsList.Remove(selected);
 
         }
 
-        private void availablePermissions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void availableOptions_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
