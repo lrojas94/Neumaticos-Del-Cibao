f9898aa30b536a38ef6e9df66f2d5b96171ef430
Edward Romero Espinal <edwardespinal16@gmail.com>
2015-12-01 17:54:57 -0400

    Fixed Issue
    - Searchbox now works properly

--- Neumaticos del Cibao/CRUD Permissions/ViewAllPermissions.xaml.cs
+++ Neumaticos del Cibao/CRUD Permissions/ViewAllPermissions.xaml.cs
@@ -1,20 +1,21 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-    using System.Windows.Threading;
+﻿using Neumaticos_del_Cibao.Apps.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
-    namespace Neumaticos_del_Cibao.CRUD_Permissions
+namespace Neumaticos_del_Cibao.CRUD_Permissions
     {
         /// <summary>
         /// Interaction logic for ViewAllPermissions.xaml
@@ -23,41 +24,27 @@
         {
 
             private Database.databaseEntities database = new Database.databaseEntities();
-            private DispatcherTimer searchTimer;
+            private TimedFunction searchFuntion;
 
             public ViewAllPermissions()
             {
                 InitializeComponent();
                 permissions.ItemsSource = database.Permissions.ToList();
-                searchTimer = new DispatcherTimer();
-                searchTimer.Tick += searchTimer_Tick;
-                searchTimer.Interval = new TimeSpan(0, 0, 0, 0, 50);
-            
+
+                Action search = () =>
+                {
+                    permissions.ItemsSource = database.PermissionSearch(name.RealText);
+                };
+
+                searchFuntion = new TimedFunction(search);
+                
             }
         
-            private void searchTimer_Tick(object sender, EventArgs e)
-            {
-                // var searcher = name.RealText.ToLower(); 
-                var searcher = name.Text;
-                if(searcher != "")
-                {
-                    var ListPermissions = database.Permissions.Where(
-                        perm => permissions.Name.ToLower().Contains(searcher)).ToList();
-                    permissions.ItemsSource = searcher;
-                }
-                else
-                {
-                    permissions.ItemsSource = database.Permissions.ToList();
-                }
-                searchTimer.Stop();
-            }
+
 
             private void name_TextChanged(object sender, TextChangedEventArgs e)
             {
-                if(!searchTimer.IsEnabled)
-                {
-                    searchTimer.Start();
-                }
+                searchFuntion.Run();   
             }
 
             private void btnAnadir_Click(object sender, RoutedEventArgs e)
